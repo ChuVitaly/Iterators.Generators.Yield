b40b37d9a717a3cc34e290bd8f7bd1ace095f62d
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-19 15:47:49 +0300

    I did first task(Вариант 2)

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ nested_list = [
     [1, 2, None],
 ]
 
-k = 0
-while k != len(nested_list):
-    s = 0
-    a = (x for x in nested_list[k])
-    while s != len(nested_list[k]):
-        print(next(a))
-        s += 1
-    k += 1
+# Вариант 1
+# k = 0
+# while k != len(nested_list):
+#     s = 0
+#     a = (x for x in nested_list[k])
+#     while s != len(nested_list[k]):
+#         print(next(a))
+#         s += 1
+#     k += 1
+
+
+# Вариант 2
+for i in (i for lst in nested_list for i in lst):
+    print(i)
 
 
 
