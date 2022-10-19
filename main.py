7e99422ba212529ab4d4f61f57fe74842dae9c61
Виталий Чухляев <vitaliy69@mail.ru>
2022-10-19 15:35:00 +0300

    I did first task

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 
+# Task1
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
+]
+
+k = 0
+while k != len(nested_list):
+    s = 0
+    a = (x for x in nested_list[k])
+    while s != len(nested_list[k]):
+        print(next(a))
+        s += 1
+    k += 1
 
 
 
 
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    pass
 
 
