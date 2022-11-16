69c8b68bd777df57538a08d450a7be7d36ca596e
Виталий Чухляев <vitaliy69@mail.ru>
2022-11-16 20:30:01 +0300

    Task 1. Version3

--- main.py
+++ main.py
@@ -6,58 +6,50 @@
 """
 # Task1
 # Вариант 1
-nested_list = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f', 'h', False],
-    [1, 2, None],
-]
-nested_list_all = []
-k = 0
-while k != len(nested_list):
-    for i in nested_list[k]:
-        nested_list_all.append(i)
-    k += 1
-
-
-
-
-class SampleIterator:
-
-    def __init__(self, nested_list_all):
-        self.nested_list_all = nested_list_all
-        self.nested_all_len = len(nested_list_all)
-        self.n = 0
-
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.n < self.nested_all_len:
-            item = self.nested_list_all[self.n]
-            self.n += 1
-            return item
-        raise StopIteration
-
-
-
-if __name__ == '__main__':
-    for item in SampleIterator(nested_list_all):
-        print(item)
-
-    flat_list = [item for item in SampleIterator(nested_list_all)]
-    print(flat_list)
+# nested_list = [
+#     ['a', 'b', 'c'],
+#     ['d', 'e', 'f', 'h', False],
+#     [1, 2, None],
+# ]
+# nested_list_all = []
+# k = 0
+# while k != len(nested_list):
+#     for i in nested_list[k]:
+#         nested_list_all.append(i)
+#     k += 1
+#
+#
+#
+#
+# class SampleIterator:
+#
+#     def __init__(self, nested_list_all):
+#         self.nested_list_all = nested_list_all
+#         self.nested_all_len = len(nested_list_all)
+#         self.n = 0
+#
+#
+#     def __iter__(self):
+#         return self
+#
+#     def __next__(self):
+#         if self.n < self.nested_all_len:
+#             item = self.nested_list_all[self.n]
+#             self.n += 1
+#             return item
+#         raise StopIteration
+#
+#
+#
+# if __name__ == '__main__':
+#     for item in SampleIterator(nested_list_all):
+#         print(item)
+#
+#     flat_list = [item for item in SampleIterator(nested_list_all)]
+#     print(flat_list)
 
 # Task1
 # Вариант 2
-nested_list = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f', 'h', False],
-    [1, 2, None],
-]
-nested_list_all = []
-
-
 
 class SampleIterator:
 
@@ -67,24 +59,48 @@ class SampleIterator:
 
     def __iter__(self):
         self.nested_list_cursor = 0
-        self.nested_list_all_cursor = 0
+        self.nested_list_all_cursor = -1
         return self
 
     def __next__(self):
-        while self.nested_list_cursor != len(nested_list):
-            for i in nested_list[self.nested_list_cursor]:
-                nested_list_all.append(i)
+        while self.nested_list_cursor != len(self.nested_list):
+            for i in self.nested_list[self.nested_list_cursor]:
+                self.nested_list_all.append(i)
             self.nested_list_cursor += 1
-        if self.nested_list_all_cursor < len(self.nested_list_all):
-            for item in self.nested_list_all:
-                self.nested_list_all_cursor += 1
+        if self.nested_list_cursor <= len(self.nested_list):
+            for i in self.nested_list:
+                self.nested_list_all.extend(i)
+            self.nested_list_cursor += 1
+        if self.nested_list_all_cursor + 1 >= len(self.nested_list_all):
+            raise StopIteration()
+        self.nested_list_all_cursor += 1
+        return self.nested_list_all[self.nested_list_all_cursor]
 
-                return item
-        raise StopIteration
+        
 
 if __name__ == '__main__':
+    nested_list = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+
     a = SampleIterator(nested_list)
     print(a)
+    for k in a:
+        print(k)
+
+
+    # --------------------- Проверка ---------------------------------
+
+    # expected_result = ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    #
+    # i = 0
+    #
+    # for item in SampleIterator(nested_list):
+    #     print('item == expected_result[i]', item == expected_result[i])
+    #     i += 1
 
 
 
@@ -109,9 +125,3 @@ if __name__ == '__main__':
 # print(list(a))
 # for x in a:
 #     print(x)
-
-
-
-
-
-
